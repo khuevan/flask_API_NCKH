6e2410b3c18d635ad8090d734a85bacaf2ad407c
khuevan <75727842+khuevan@users.noreply.github.com>
2022-09-12 17:39:17 +0700

    commit

--- main.py
+++ main.py
@@ -3,23 +3,32 @@ import datetime
 from flask import Flask, request, jsonify, stream_with_context
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from pymongo import MongoClient
+import json
+from bson.json_util import dumps, loads
 
 
 app = Flask(__name__)
 jwt = JWTManager(app)
-app.config['JWT_SECRET_KEY'] = ''
+app.config['JWT_SECRET_KEY'] = '8Zz5tw0Ionm3XPZZfN0NOml3z9FMfmpgXwovR9fp6ryDIoGRM8EPHAB6iHsc0fb'
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=1)
 
-client = MongoClient("mongodb://localhost:27017/") 
+client = MongoClient("mongodb://localhost:27017/")
 db = client["pinaapple"]
 users_collection = db["users"]
+images_collection = db['images']
+videos_collection = db['videos']
+discovery_collection = db['discovery']
 
+@app.route('/')
+def index():
+    return jsonify({'msg': 'OK'}), 200
 
 @app.route("/api/register", methods=["POST"])
 def register():
 	new_user = request.get_json()
 	new_user["password"] = hashlib.sha256(new_user["password"].encode("utf-8")).hexdigest() 
-	doc = users_collection.find_one({"username": new_user["username"]}) 
+	# new_user.update({"name": "","permission": 0,"avatar": "","email": "","phone": ""})
+	doc = users_collection.find_one({"account": new_user["account"]})
 	if not doc:
 		users_collection.insert_one(new_user)
 		return jsonify({'msg': 'User created successfully'}), 201
@@ -30,12 +39,12 @@ def register():
 @app.route("/api/login", methods=["POST"])
 def login():
 	login_details = request.get_json()
-	user_from_db = users_collection.find_one({'username': login_details['username']})  
+	user_from_db = users_collection.find_one({'account': login_details['account']})  
 
 	if user_from_db:
 		encrpted_password = hashlib.sha256(login_details['password'].encode("utf-8")).hexdigest()
 		if encrpted_password == user_from_db['password']:
-			access_token = create_access_token(identity=user_from_db['username'])
+			access_token = create_access_token(identity=user_from_db['account'])
 			return jsonify(access_token=access_token), 200
 
 	return jsonify({'msg': 'The username or password is incorrect'}), 401
@@ -45,7 +54,7 @@ def login():
 @jwt_required
 def profile():
 	current_user = get_jwt_identity()
-	user_from_db = users_collection.find_one({'username' : current_user})
+	user_from_db = users_collection.find_one({'account' : current_user})
 	if user_from_db:
 		del user_from_db['_id'], user_from_db['password']
 		return jsonify({'profile' : user_from_db }), 200
@@ -53,18 +62,93 @@ def profile():
 		return jsonify({'msg': 'Profile not found'}), 404
 
 
-@app.route("/api/streamcontex", methods=['POST'])
+@app.route("/api/streamcontex")
 def camera():
     def gen():
         # get image from request ?
         # process ?
-        # yield ?
+        # yield ? 
         # return ?
-        yield '?'
+        yield '!'
         yield 'work?'
         yield 'dunno'
+
     return app.response_class(stream_with_context(gen()))
 
 
+@app.route('/api/album-images')
+def album_images():
+	data = images_collection.find()
+
+	def del_item(item):
+		# for item in data:
+		del item['_id']   
+		del item['account']
+		del item['accuracy']
+		del item['class-name']
+		# print(type(data[0]))
+		return item
+	data = [del_item(item) for item in data]
+	return jsonify(data)
+
+
+def insert_images(account, img_name, date, model, class_name, accuracy, is_count, is_cutout, path):
+	data = {
+		'account': account,
+		'img-name': img_name,
+		'date': date,
+		'model': model,
+		'class-name': class_name,
+		'accuracy': accuracy,
+		'is-cutout': is_cutout,
+		'is-count': is_count,
+		'path': path,
+	}
+	images_collection.insert_one(data)
+	return "success"
+
+
+def insert_discover(account, title, content, path, category, model):
+	data = {
+		'account': account,
+		'title': title,
+		'content': content,
+		'path': path,
+		'category': category,
+		'model': model,
+	}
+	discovery_collection.insert_one(data)
+	return "ok"
+
+
+@app.route('/api/discovery/characteristics/<model>')
+def album_images(model):
+	data = []
+	for item in discovery_collection.find({'category':'characteristics', 'model': {model}}):
+		new_item = {
+			'title': item['title'],
+			'path': item['path'],
+			'model': item['model']
+		}
+		data.append(new_item)
+
+	return jsonify(data)
+
+
+@app.route('/api/discovery/<category>')
+def album_images(category):
+	data = [] 
+	for item in discovery_collection.find({'category':{category}}):
+		new_item = {
+			'title': item['title'],
+			'path': item['path'],
+			'account': item['account'],
+			'content': item['content']
+		}
+		data.append(new_item)
+
+	return jsonify(data)
+
+
 if __name__ == '__main__':
-	app.run()
+	app.run(host='0.0.0.0',debug=True)
---
+++ test.py
@@ -0,0 +1,6 @@
+from main import insert_images
+
+
+for i in range(13):
+    path = 'http://127.0.0.1:5000/static/album-images/image{}.jpg'.format(i+1)
+    insert_images("thien", "image"+str(i+1), "12/09/2022", "pineaple", [], None, False, False, path)
