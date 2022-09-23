aaf0d44c00af7d8affa748b31762804b05119555
khuevan <75727842+khuevan@users.noreply.github.com>
2022-09-23 11:30:43 +0700

    commit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import hashlib
 import redis
 import datetime
-from flask import Flask, request, jsonify, stream_with_context
+from flask import Flask, request, jsonify
+from flask_socketio import SocketIO, emit
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from pymongo import MongoClient
 import json
@@ -11,6 +12,8 @@ from setting import JWT_SECRET_KEY, MONGODB_STRING, DEBUG, HOST, PORT
 
 app = Flask(__name__)
 jwt = JWTManager(app)
+socketio = SocketIO(app)
+
 
 ACCESS_EXPIRES = datetime.timedelta(hours=1)
 app.config['JWT_SECRET_KEY'] = JWT_SECRET_KEY
@@ -197,5 +200,32 @@ def detect_img():
 		return jsonify({'msg': 'no permission'}), 405
 
 
+@socketio.on('image')
+def image(data_image):
+    sbuf = StringIO()
+    sbuf.write(data_image)
+
+    # decode and convert into image
+    b = io.BytesIO(base64.b64decode(data_image))
+    pimg = Image.open(b)
+
+    # converting RGB to BGR, as opencv standards
+    frame = cv2.cvtColor(np.array(pimg), cv2.COLOR_RGB2BGR)
+
+    # Process the image frame
+    frame = imutils.resize(frame, width=700)
+    frame = cv2.flip(frame, 1)
+    imgencode = cv2.imencode('.jpg', frame)[1]
+
+    # base64 encode
+    stringData = base64.b64encode(imgencode).decode('utf-8')
+    b64_src = 'data:image/jpg;base64,'
+    stringData = b64_src + stringData
+
+    # emit the frame back
+    emit('response_back', stringData)
+
+
 if __name__ == '__main__':
-	app.run(host=HOST, port=PORT, debug=DEBUG)
+	# app.run(host=HOST, port=5001, debug=DEBUG)
+	socketio.run(app=app, host=HOST, port=PORT, debug=DEBUG)
--- test.py
+++ test.py
@@ -1,7 +1,8 @@
-from main import insert_images
+# from main import insert_images
 
-from datetime import date
+# from datetime import date
 
-# for i in range(13):
-#     path = 'http://127.0.0.1:5000/static/album-images/image{}.jpg'.format(i+1)
-#     insert_images("thien", "image"+str(i+1), "12/09/2022", "pineaple", [], None, False, False, path)
+# # for i in range(13):
+# #     path = 'http://127.0.0.1:5000/static/album-images/image{}.jpg'.format(i+1)
+# #     insert_images("thien", "image"+str(i+1), "12/09/2022", "pineaple", [], None, False, False, path)
+
